f25e0547763d3b2c5bbef7e55db2632da493f6a6
Phoomparin Mano <poom@hey.com>
2021-08-29 19:16:46 +0700

    chore: outline

---
+++ notes/29-aug.ts
@@ -0,0 +1,24 @@
+// Why TypeScript?
+
+// You have been using TypeScript all along.
+
+// - 3 Primitives: Variable Types & Inference
+// - Functions
+// - Arrays vs Tuples
+
+// Any, Unknown, Never, Void
+// Null, Undefined
+// Inline object typing (fixed keys, string keys; Index Types and Index Signatures)
+// Shaping the data model with Interface and Type
+// Optionals
+// Type Casting: As and As Unknown
+// Enums vs Enums with String Values
+// Union and Intersection Types (& Discriminated Unions)
+// String Unions
+// Type Narrowing and Widening
+// Strict Mode
+// Generic Types
+// Using Generics
+// Common TS Helper Types (Generics) & How to read generics
+   // Record, Partial, Readonly, Omit, Pick, Exclude, etc.
+// Readonly in Interface
